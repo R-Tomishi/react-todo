e8537cc6bfe1590fc2a511a8f218469e0514b08a
R-Tomishi <43924379+R-Tomishi@users.noreply.github.com>
2022-03-28 12:24:39 +0900

    0

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div></div>
+      <input placeholder="TODOを入力" />
+      <button>追加</button>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>aaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
+};
